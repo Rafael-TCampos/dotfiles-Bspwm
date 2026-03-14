1634f0e2cebf9ea5300e1d7da1bfbd6b81b750ec
Rafael-TCampos <rafael-cotonete2011@hotmail.com>
2026-03-14 00:07:21 -0300

    update readme and install ascript

---
+++ README.md
@@ -0,0 +1,43 @@
+# Dotfiles BSPWM
+
+Minhas configurações Linux usando **bspwm**, **sxhkd**, **kitty**, **picom**, **polybar** e **LazyVim**.
+
+## Programas utilizados
+
+* bspwm
+* sxhkd
+* kitty
+* picom
+* polybar
+* feh
+* neovim (LazyVim)
+
+## Instalação
+
+Clone o repositório:
+
+```bash
+git clone https://github.com/Rafael-TCampos/dotfiles-Bspwm.git
+cd dotfiles-Bspwm
+```
+
+Execute o script de instalação:
+
+```bash
+chmod +x install.sh
+./install.sh
+```
+
+## Estrutura
+
+```
+dotfiles-bspwm
+ ├─ bspwm
+ ├─ sxhkd
+ ├─ kitty
+ ├─ picom
+ ├─ polybar
+ ├─ nvim
+ └─ install.sh
+```
+
--- install.sh
+++ install.sh
@@ -1,42 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Atualizando sistema..."
-sudo apt update && sudo apt upgrade -y
+echo "Instalando programas..."
 
-echo "Instalando programas essenciais..."
+sudo apt update
+
 sudo apt install -y \
 bspwm \
 sxhkd \
 kitty \
 picom \
+polybar \
 feh \
-git \
-curl \
-build-essential \
-unzip \
 neovim \
-ripgrep \
-fd-find
+git \
+curl
 
-echo "Instalando Ruby..."
-sudo apt install -y ruby-full
+echo "Criando pastas..."
 
-echo "Instalando Rubocop..."
-gem install rubocop
-
-echo "Criando pasta de config..."
 mkdir -p ~/.config
 
-echo "Linkando dotfiles..."
+echo "Copiando configurações..."
 
-ln -sf ~/dotfiles/bspwm ~/.config/bspwm
-ln -sf ~/dotfiles/sxhkd ~/.config/sxhkd
-ln -sf ~/dotfiles/kitty ~/.config/kitty
-ln -sf ~/dotfiles/picom ~/.config/picom
-ln -sf ~/dotfiles/nvim ~/.config/nvim
+cp -r bspwm ~/.config/
+cp -r sxhkd ~/.config/
+cp -r kitty ~/.config/
+cp -r picom ~/.config/
+cp -r polybar ~/.config/
+cp -r nvim ~/.config/
 
-echo "Instalando LazyVim..."
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-echo "Instalação finalizada!"
-echo "Reinicie a sessão para carregar o bspwm."
+echo "Instalação concluída!"
