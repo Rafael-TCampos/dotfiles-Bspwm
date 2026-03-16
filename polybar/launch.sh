c8803243f0c21e917d574c8a33d5ddc543d42ae6
Rafael-TCampos <rafael-cotonete2011@hotmail.com>
2026-03-15 21:46:18 -0300

    Atualizacoa polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+# Barra principal no monitor HDMI-0
+MONITOR=HDMI-0 polybar example &
 
-killall -q polybar
-
-polybar example &
+# Barra secundária no monitor DVI-0 (vertical)
+MONITOR=DVI-0 polybar secondary &
