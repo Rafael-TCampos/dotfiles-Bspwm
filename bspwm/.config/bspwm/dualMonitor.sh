387b15e639cb7ff6951fc22cf861c634af0f9fd5
Rafael-TCampos <rafael-cotonete2011@hotmail.com>
2026-08-29 15:43:14 -0300

    Modificado o dualMonitor.sh para modo retrato

--- bspwm/.config/bspwm/dualMonitor.sh
+++ bspwm/.config/bspwm/dualMonitor.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 xrandr \
   --output DisplayPort-0 --off \
-  --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output DVI-D-0 --mode 1440x900 --pos 198x1080 --rotate normal \
+  --output DVI-D-0 --mode 1440x900 --pos 0x0 --rotate right \
+  --output HDMI-A-0 --mode 1920x1080 --pos 900x0 --rotate normal \
   --output DVI-I-1 --off
