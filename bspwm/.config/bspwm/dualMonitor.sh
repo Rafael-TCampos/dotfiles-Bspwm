80d0f1b334bc8fbe1599321e0b3ec82bdf153ef2
Rafael-TCampos <rafael-cotonete2011@hotmail.com>
2026-09-06 19:53:08 -0300

    Modificado o dualMonitor

--- bspwm/.config/bspwm/dualMonitor.sh
+++ bspwm/.config/bspwm/dualMonitor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xrandr \
-  --output DisplayPort-0 --mode 900x1440 --pos 0x0 --rotate left\
+  --output DisplayPort-0 --mode 1440x900 --pos 0x0 --rotate left \
   --output HDMI-A-0 --mode 1920x1080 --pos 900x0 --rotate normal \
   --output DVI-D-0 --off \
   --output DVI-I-1 --off
